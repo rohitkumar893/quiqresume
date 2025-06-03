96bd1e6834bf3a502289a215b78ae2762ae92fca
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-06-03 15:01:58 +0530

    worked on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,30 @@ function App() {
 
   return (
     <>
-      <main h-full w-full>
+      <main className='h-full w-full'>
         
 
-        <div className='flex w-full h-screen flex-col'>
-          <nav className='navbar h-[100px] w-full'>
-        </nav>
+        <div className='flex w-full h-full flex-col'>
+          <nav className='navbar h-[100px] w-full flex justify-center items-center'>
+            <h1 className='text-[30px] font-medium'>BuildMyRes.</h1>
+          </nav>
           
-          <div className="content flex w-full h-full">
-              <div className="first h-full w-full">
-                
+          <div className="content flex flex-col sm:flex-row w-full h-full">
+              <div className="first h-full w-full flex items-center justify-center relative">
+                <img src="bgdesign.png" className='sm:w-[600px] w-[350px]' />
+                <img src="resdemo.jpg" className='z-[3] h-[350px] absolute rounded-md' />
               </div>
 
-              <div className="second flex flex-col items-start justify-center h-full w-full gap-[20px]">
-                  <h1 className='text-[30px] font-bold px-[50px]'>Fast, Clean, and Effective<br />Resume Creation</h1>
-                  <p className='text-[20px] font-medium px-[50px]'>Create a resume that matches your skill and experience, in no time.</p>
-                  <button className='text-[35px] px-[50px] font-bold'>BUILD MY RESUME</button>
+              <div className="second flex flex-col items-start justify-center h-full w-full sm:gap-[30px] gap-[24px]">
+                <button className='text-[40px] px-[50px] font-bold'>BUILD<img src="arrow.png" className=' h-[50px]' /></button>
+                  <h1 className='sm:text-[30px] text-[28px] font-bold px-[50px]'>Fast and Clean Resume Creation</h1>
+                  <p className='text-[22px] font-medium px-[50px] w-[500px]'>Create a resume that matches your skill and experience, in no time.</p>
+                  </div>
               </div>
-              </div>
+        </div>
+
+        <div className="about h-[100%]">
+          <h1></h1>
         </div>
       </main>
     </>
