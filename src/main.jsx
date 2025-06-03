19cdf796a470471b304bea0f3774d33393620303
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-06-03 20:17:45 +0530

    created create route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Create from './pages/Create';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/create" element={<Create />}></Route>
+    </Routes>
+  </BrowserRouter>,
 )
