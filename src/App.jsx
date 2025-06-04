f77c51170fdf6864c69611d012b196db27939b3a
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-06-04 09:55:15 +0530

    adjusted about section

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
               </div>
         </div>
 
-        <div className="about h-full w-full flex flex-col mt-[65px] justify-start items-center">
+        <div className="about h-full w-full flex flex-col mt-[85px] justify-start items-center">
           <h1 className='text-center text-[40px]'>ABOUT</h1>
           <p className='text-center text-[28px] w-[90%] sm:w-[85%]'>A straightforward web app that helps users create clean, professional resumes quickly. Users can input their personal details, work experience, education, and skills, then generate a downloadable resume in PDF format. No complicated features-just a fast, easy way to get your resume ready for job hunting.</p>
         </div>
