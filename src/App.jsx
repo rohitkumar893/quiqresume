a039d5dfef901be1cbb8e49e38507f57456b8e62
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-07-18 22:41:42 +0530

    update: text responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
         <div className="about h-full w-full flex flex-col mt-[85px] justify-start items-center">
           <h1 className='text-center text-[40px]'>ABOUT</h1>
-          <p className='text-center text-[28px] w-[90%] sm:w-[85%]'>A straightforward web app that helps users create clean, professional resumes quickly. Users can input their personal details, work experience, education, and skills, then generate a downloadable resume in PDF format. No complicated features-just a fast, easy way to get your resume ready for job hunting.</p>
+          <p className='text-center text-[20px] sm:text-[28px] w-[90%] sm:w-[85%]'>A straightforward web app that helps users create clean, professional resumes quickly. Users can input their personal details, work experience, education, and skills, then generate a downloadable resume in PDF format. No complicated features-just a fast, easy way to get your resume ready for job hunting.</p>
         </div>
       </main>
     </>
