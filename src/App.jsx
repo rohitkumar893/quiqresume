27199e1f986a77ad0101bfdcafabbb715912fd4d
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-07-30 10:38:12 +0530

    feat: form

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
               <div className="second flex flex-col items-center md:items-start justify-center h-full w-full">
                   <div className='flex flex-col items-center md:items-start gap-[50px]'>
                     <p className='p-[38px] mt-[-120px] md:mt-0 md:p-0 text-[32px] md:text-[55px] font-semibold w-[500px] leading-[1.2]'>Create <span className='text-sky-600'>resume</span> that matches your skills and experience, in no time.</p>
-                    <Link to="/create" className='text-[30px] mt-[-20px] md:mt-0 text-center text-white px-4 font-bold cursor-pointer hover:scale-104 transition-transform bg-sky-600 w-[180px] h-[62px] flex items-center justify-center rounded-[10px]'>GENERATE</Link>
+                    <Link to="/create" className='text-[30px] mt-[-20px] md:mt-0 text-center text-white px-4 font-bold cursor-pointer hover:scale-104 transition-transform bg-sky-600 w-[180px] h-[65px] flex items-center justify-center rounded-[10px]'>GENERATE</Link>
                   </div>
                 </div>
             </div>
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -7,50 +7,41 @@ const Create = () => {
     <Navbar />
     <div className='flex flex-col mt-[-50px] justify-center items-center h-full w-full'>
       <div className='h-[490px] sm:h-[520px] w-[390px] sm:w-[440px] flex-col gap-[30px] flex items-center justify-center rounded-[10px] shadow-xl bg-white'>
-          <form className="flex flex-col gap-4">
+          <h1 className='text-[24px] font-medium'>Fill your details</h1>
+          <form className="flex flex-col gap-5">
               <div className="flex items-center gap-2">
                 <label htmlFor="name" className="w-32">Name</label>
-                <input type="text" id="name" className="border w-48 px-[5px]" />
+                <input type="text" id="name" className="border w-48 px-[5px]" placeholder='Full Name' />
               </div>
 
               <div className="flex items-center gap-2">
                 <label htmlFor="contact" className="w-32">Contact</label>
-                <input type="number" id="contact" className="border w-48 px-[5px]" />
+                <input type="number" id="contact" className="border w-48 px-[5px]" placeholder='Contact Number' />
               </div>
 
               <div className="flex items-center gap-2">
                 <label htmlFor="email" className="w-32">Email</label>
-                <input type="text" id="email" className="border w-48 px-[5px]" />
+                <input type="text" id="email" className="border w-48 px-[5px]" placeholder='Your e-mail' />
               </div>
 
               <div className="flex items-center gap-2">
-                <label htmlFor="occupation" className="w-32">Occupation</label>
-                <input type="text" id="occupation" className="border w-48 px-[5px]" />
-              </div>
-
-              <div className="flex items-center gap-2">
-                <label htmlFor="university" className="w-32">University</label>
-                <input type="text" id="university" className="border w-48 px-[5px]" />
-              </div>
-
-              <div className="flex items-center gap-2">
-                <label htmlFor="qualification" className="w-32">Qualification</label>
-                <input type="text" id="qualification" className="border w-48 px-[5px]" />
+                <label htmlFor="education" className="w-32">Education</label>
+                <input type="text" id="education" className="border w-48 px-[5px]" placeholder='Course, University, Year' />
               </div>
 
               <div className="flex items-center gap-2">
                 <label htmlFor="skills" className="w-32">Skills</label>
-                <input type="text" id="skills" className="border w-48 px-[5px]" />
+                <input type="text" id="skills" className="border w-48 px-[5px]" placeholder='Separate by commas' />
               </div>
 
               <div className="flex items-center gap-2">
                 <label htmlFor="experience" className="w-32">Experience</label>
-                <input type="text" id="experience" className="border w-48 px-[5px]" />
+                <input type="text" id="experience" className="border w-48 px-[5px]" placeholder='Role at companies, duration' />
               </div>
 
               <div className="flex items-center gap-2">
                 <label htmlFor="address" className="w-32">Address</label>
-                <input type="text" id="address" className="border w-48 px-[5px]" />
+                <input type="text" id="address" className="border w-48 px-[5px]" placeholder='City, State' />
               </div>
 
               <button className='bg-sky-500 h-[40px] text-white mt-[10px] font-medium cursor-pointer'>GENERATE</button>
