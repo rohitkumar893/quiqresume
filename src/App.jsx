c72248ddf6cccf28ff62ccaf79ee73febf11caea
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-08-03 23:16:38 +0530

    add: hero icon

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,13 @@ function App() {
 
           <div className="content flex flex-col md:flex-row w-full h-full">
               <div className="first h-full w-full flex items-center justify-center relative">
-                <img src="resdemo3.png" className='heroimage object-contain z-[3] md:h-[540px] h-[252px] absolute rounded-md hover:scale-104 transition-transform' />
+                <img src="resdemo.png" className='heroimage object-contain z-[3] md:h-[500px] h-[232px] absolute rounded-md hover:scale-104 transition-transform' />
               </div>
 
               <div className="second flex flex-col items-center md:items-start justify-center h-full w-full">
                   <div className='flex flex-col items-center md:items-start gap-[50px]'>
                     <p className='p-[38px] mt-[-120px] md:mt-0 md:p-0 text-[32px] md:text-[55px] font-semibold w-[500px] leading-[1.2]'>Create <span className='text-sky-600'>resume</span> that matches your skills and experience, in no time.</p>
-                    <Link to="/create" className='text-[30px] mt-[-20px] md:mt-0 text-center text-white px-4 font-bold cursor-pointer hover:scale-104 transition-transform bg-sky-600 w-[180px] h-[65px] flex items-center justify-center rounded-[10px]'>GENERATE</Link>
+                    <Link to="/create" className='text-[30px] mt-[-20px] md:mt-0 text-center text-white px-4 font-bold cursor-pointer hover:scale-104 transition-transform bg-sky-600 w-[235px] h-[65px] flex items-center justify-center rounded-[10px]'>GET STARTED</Link>
                   </div>
                 </div>
             </div>
