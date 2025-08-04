03b1cf43a726592e2464743b639ddc749de7497c
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-08-04 22:15:14 +0530

    feat: home button

--- components/About.jsx
+++ components/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const About = () => {
   return (
      <div className="about h-full w-full flex flex-col pt-[80px] mt-[85px] justify-start items-center">
         <h1 className='text-center text-[38px]'><b>ABOUT</b></h1>
         <p className='text-center text-[20px] sm:text-[28px] w-[90%] sm:w-[85%]'>A straightforward web app that helps users create clean, professional resumes quickly. Users can input their personal details, work experience, education, and skills, then generate a downloadable resume in PDF format. No complicated features-just a fast, easy way to get your resume ready for job hunting.</p>
+        <Link to='/' className='mt-[50px] text-[20px] bg-gray-200 w-[70px] text-center rounded-[10px]'>Home</Link>
     </div>
   )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className='navbar h-[80px] w-full justify-between bg-gray-400 flex items-center'>
-        <Link to="/"><h1 className='text-[28px] font-medium ml-[140px] text-white'>QuiqResume</h1></Link>
-        <Link to="/about"><h1 className='text-[28px] font-medium mr-[120px] text-white'>About</h1></Link>
+    <nav className='navbar h-[80px] w-full justify-between bg-gray-300 flex items-center'>
+        <Link to="/"><h1 className='text-[25px] font-medium ml-[50px] text-gray-800'>QuiqResume</h1></Link>
+        <Link to="/about"><h1 className='text-[25px] font-medium mr-[50px] text-gray-800'>About</h1></Link>
     </nav>
   )
 }
